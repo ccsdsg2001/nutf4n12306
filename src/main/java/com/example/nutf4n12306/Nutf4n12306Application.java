2602b4332375924ece3f45089a0f67f49f5f5288
NutF4n <ccsdsg2016@gmail.com>
2023-08-20 18:27:49 +0800

    12306

---
+++ src/main/java/com/example/nutf4n12306/Nutf4n12306Application.java
@@ -0,0 +1,13 @@
+package com.example.nutf4n12306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nutf4n12306Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nutf4n12306Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nutf4n12306/Nutf4n12306ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutf4n12306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nutf4n12306ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

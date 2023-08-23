cc8fd8edcba667fa8860eb8eb277fb6aac3caf02
NutF4n <ccsdsg2016@gmail.com>
2023-08-23 18:36:29 +0800

    vue

--- member/src/main/java/com/example/controller/Membercontroller.java
+++ member/src/main/java/com/example/controller/Membercontroller.java
@@ -1,14 +1,14 @@
 package com.example.controller;
 
 import com.example.R.CommonResp;
+import com.example.R.MemberLoginReq;
+import com.example.R.MemberSendCodeReq;
 import com.example.R.memberRegisterRequest;
+import com.example.resp.MemberLoginResp;
 import com.example.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author cc
@@ -35,5 +35,21 @@ public class Membercontroller {
         long register = memberService.register(mobile);
 
         return new CommonResp<>(register);
+    }
+
+    @PostMapping("/send-code")
+    public CommonResp<Long> sedncode(@Valid @RequestBody MemberSendCodeReq mobile){
+
+
+        memberService.sendcode(mobile);
+
+        return new CommonResp<>();
+    }
+
+    @PostMapping("/login")
+    public CommonResp<MemberLoginResp> login(@Valid MemberLoginReq mobile){
+        MemberLoginResp login = memberService.login(mobile);
+
+        return new CommonResp<>(login);
     }
 }
--- member/src/main/java/com/example/service/MemberService.java
+++ member/src/main/java/com/example/service/MemberService.java
@@ -1,15 +1,23 @@
 package com.example.service;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.IdUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.example.R.MemberLoginReq;
+import com.example.R.MemberSendCodeReq;
 import com.example.R.memberRegisterRequest;
 import com.example.domain.Member;
 import com.example.domain.MemberExample;
 import com.example.exception.BusinessException;
 import com.example.exception.BusinessExceptionEnum;
 import com.example.mapper.MemberMapper;
+import com.example.resp.MemberLoginResp;
 import com.example.util.SnowUtil;
 import jakarta.annotation.Resource;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,26 +29,26 @@ import java.util.List;
 @Service
 public class MemberService {
 
+
+    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
     @Resource
     private MemberMapper memberMapper;
 
-    public int count(){
+    public int count() {
         return Math.toIntExact(memberMapper.countByExample(null));
     }
 
 
-    public long register(memberRegisterRequest mobile){
-        String mobile1 = mobile.getMobile();
+    public long register(memberRegisterRequest req) {
+        // TODO:手机号注册登录接口（阿里云），第一次登录失败显示验证码。 检验短信频繁，保存短信登录表
+        String mobile1 = req.getMobile();
 
 
-
-        MemberExample memberExample = new MemberExample();
-        memberExample.createCriteria().andMobileEqualTo(mobile1);
-        List<Member> members = memberMapper.selectByExample(memberExample);
+        Member member1 = selectByMobile(mobile1);
         //members 为null yichang
-        if(CollUtil.isEmpty(members)){
+        if (ObjectUtil.isNotNull(member1)) {
 //            return members.get(0).getId();
-            throw  new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
 
 
@@ -51,4 +59,59 @@ public class MemberService {
         return member.getId();
     }
 
+    private Member selectByMobile(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if (CollUtil.isEmpty(list)) {
+            return null;
+        } else {
+            return list.get(0);
+        }
+    }
+
+
+    public void sendcode(MemberSendCodeReq req) {
+        String mobile = req.getMobile();
+        Member member1 = selectByMobile(mobile);
+        if (ObjectUtil.isNull(member1)) {
+            LOG.info("手机号不存在");
+            Member member = new Member();
+            member.setId(SnowUtil.getSnowflakeNextId());
+            member.setMobile(mobile);
+            memberMapper.insert(member);
+        } else {
+            LOG.info("手机号存在");
+        }
+
+        //code
+//        String code = RandomUtil.randomString(4);
+        String code = "8888";
+        LOG.info("生成短信验证码", code);
+        //TODO:保存短信记录表：手机号，短信验证码，有效期，是否已经使用，业务类型，发送时间，使用时间
+        LOG.info("保存短信记录表");
+        //TODO:对接短信通道，发送通道，阿里云发送短信
+        LOG.info("对接短信通道，发送短信");
+
+
+    }
+
+    public MemberLoginResp login(MemberLoginReq req) {
+        String mobile = req.getMobile();
+        String code = req.getCode();
+        Member member1 = selectByMobile(mobile);
+        if (ObjectUtil.isNull(member1)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_NOT_EXIST);
+        }
+
+        //检验短信验证码 TODO:这里先写死,倒是或在修改
+        if(!"8888".equals(code)){
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
+        }
+
+
+        return BeanUtil.copyProperties(member1, MemberLoginResp.class);
+
+
+    }
 }

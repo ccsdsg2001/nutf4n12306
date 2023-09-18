80a7179644c652e266d42aa4459ec3cdf0a5416e
NutF4n <ccsdsg2016@gmail.com>
2023-09-18 16:56:00 +0800

    admin_12306

---
+++ admin-12306/src/views/main/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>NutF4n-admin</h1>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+<style>
+</style>
---
+++ admin-12306/src/views/main/welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>NutF4n-12306-admin后台</h1>
+</template>
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+<style>
+</style>
